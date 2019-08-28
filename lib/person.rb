106f0f3318eb932bde48e96e24c4519f3b40bd66
Tom White <tomjameswhite94@gmail.com>
2019-08-28 15:04:14 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -11,3 +11,25 @@ def name
 end
 
 end
+
+class Dog 
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+    
+    def breed=(breed)
+      @breed = breed
+    end
+    
+    def breed 
+      @breed
+    end
+  end
+  
